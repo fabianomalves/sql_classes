23621581605f172baac893a6f5479d8e40052ec7
fabianomalves <fabianomalves@proton.me>
2022-12-17 17:34:58 -0300

    alter sql files, making one file. removing other files.

--- ep03_case_when_like_coalesce/coalesce.sql
+++
@@ -1,4 +0,0 @@
-select distinct
-           coalesce(product_category_name, 'outros') as categoria_fillna
-
-from tb_products
--- ep03_case_when_like_coalesce/distinct.sql
+++
@@ -1,7 +0,0 @@
--- lista das categorias por ordem alfabética
-
-select distinct product_category_name
-
-from tb_products
-
-order by product_category_name
