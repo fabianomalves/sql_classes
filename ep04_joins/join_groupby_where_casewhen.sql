073ffba176fc53395b94fa9fd17769c6dd00bb80
fabianomalves <fabianomalves@proton.me>
2022-12-21 08:49:02 -0300

    Altered file and add new query example with left join and case when

---
+++ ep04_joins/join_groupby_where_casewhen.sql
@@ -0,0 +1,181 @@
+/* What's the total revenue for each product category?
+ * What are the total sales?
+ * What's the total of units, and orders?
+ */
+
+-- Select products table
+SELECT
+    *
+FROM
+    tb_products AS tp
+LIMIT 10;
+-----------------------------
+
+-- Select orders table 
+SELECT
+    *
+FROM
+    tb_orders AS to1
+LIMIT 10;
+-----------------------------
+
+-- Select orders items table 
+SELECT *
+FROM tb_order_items AS toi
+LIMIT 10;
+-----------------------------
+
+/*Making a join and bring only product category name 
+ * from the table 2
+ */
+SELECT
+    t1.order_id,
+    t1.order_item_id,
+    t1.product_id,
+    t2.product_category_name,
+    t1.price 
+FROM
+    tb_order_items AS t1
+LEFT JOIN tb_products AS t2 
+ON
+    t1.product_id = t2.product_id
+-----------------------------
+
+/*Now using the GROUP BY the product_category_name. 
+ * In example, there is the query with join,
+ * filtering and bring the 10 best product category, 
+ * by revenue
+ * */ 
+SELECT
+    t2.product_category_name,
+    SUM(t1.price) AS revenue
+FROM
+    tb_order_items AS t1
+LEFT JOIN tb_products AS t2 
+ON
+    t1.product_id = t2.product_id
+GROUP BY    
+    t2.product_category_name
+ORDER BY
+    SUM(t1.price) DESC
+LIMIT 10;
+-----------------------------
+
+--Using a COUNT and exhibiting the counting of total sales items
+SELECT
+    t2.product_category_name,
+    SUM(t1.price) AS revenue,
+    COUNT(*) AS total_items_sales
+FROM
+    tb_order_items AS t1
+LEFT JOIN tb_products AS t2 
+ON
+    t1.product_id = t2.product_id
+GROUP BY    
+    t2.product_category_name
+ORDER BY
+    SUM(t1.price) DESC
+-----------------------------
+
+SELECT t2.product_category_name, SUM(t1.price) AS revenue,
+-- Using COUNT, to bring all itens sales
+COUNT(*) AS total_items_sales,
+-- Using a DISTINCT, to bring back only distinct orders
+COUNT(DISTINCT t1.order_id) AS quant_orders,
+--Dividing the total sales by quantity order and using CAST to
+--transform integer into float.
+--Using round to limit the number in 2 decimals
+ROUND(COUNT(*) / CAST(COUNT(DISTINCT t1.order_id) AS FLOAT), 2) AS average_items_by_orders
+FROM tb_order_items AS t1
+LEFT JOIN tb_products AS t2 
+ON t1.product_id = t2.product_id
+GROUP BY    
+    t2.product_category_name
+ORDER BY COUNT(*) / CAST(COUNT(DISTINCT t1.order_id) AS FLOAT) DESC
+-----------------------------   
+
+SELECT
+    *
+FROM
+    tb_orders AS to1;
+-----------------------------   
+
+/* What's the total revenue for each product category?
+ * What are the total sales?
+ * What's the total of units, and orders?
+ * Considering only the sales from 2017, and delivered orders 
+ */
+
+SELECT
+    t2.product_category_name,
+    SUM(t1.price) AS revenue,
+    -- Using COUNT, to bring all itens sales
+    COUNT(*) AS total_items_sales,
+    -- Using a DISTINCT, to bring back only distinct orders
+    COUNT(DISTINCT t1.order_id) AS quant_orders,
+    --Dividing the total sales by quantity order and using CAST to
+    --transform integer into float.
+    --Using round to limit the number in 2 decimals
+    ROUND(
+        COUNT(*) / CAST(
+            COUNT(DISTINCT t1.order_id) AS FLOAT
+        ),
+        2
+    ) AS average_items_by_orders
+FROM
+    tb_order_items AS t1
+LEFT JOIN tb_products AS t2 
+ON
+    t1.product_id = t2.product_id
+LEFT JOIN tb_orders AS t3
+ON
+    t1.order_id = t3.order_id
+WHERE
+    t3.order_status = 'delivered'
+    AND STRFTIME('%Y', t3.order_approved_at) = '2017'
+GROUP BY
+    t2.product_category_name
+ORDER BY
+    COUNT(*) / CAST(
+        COUNT(DISTINCT t1.order_id) AS FLOAT
+    ) DESC
+-----------------------------   
+
+/* What sellers didn't sale in December 2017? 
+ * Filter by state
+ *  Consider the whole base, only with delivered requests. 
+ */
+SELECT
+    t2.seller_id,
+    t3.seller_state,
+    --The max return the maximum transation between 0 and 1
+    --and return the 1
+    MAX(CASE
+        WHEN STRFTIME('%Y-%m', t1.order_approved_at) = '2017-12' THEN 1
+        ELSE 0
+    END) AS flag_compra_12_2017,
+    t2.seller_id
+FROM
+    tb_orders AS t1
+LEFT JOIN tb_order_items AS t2
+ON
+    t1.order_id = t2.order_id
+LEFT JOIN tb_sellers AS t3
+ON
+    t3.seller_id = t2.seller_id
+WHERE
+    t1.order_approved_at < '2018-01-01'
+    AND t1.order_status = 'delivered'
+GROUP BY
+    t2.seller_id,
+    t3.seller_state
+HAVING
+    MAX(CASE
+        WHEN STRFTIME('%Y-%m', t1.order_approved_at) = '2017-12' THEN 1
+        ELSE 0
+    END) = 0
+-----------------------------   
+
+
+
+
